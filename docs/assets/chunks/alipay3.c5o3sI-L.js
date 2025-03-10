e59b0f3724ebdb299df116a0554211f8d16187aa
plus <29817353+chiaweilee@users.noreply.github.com>
2025-03-10 21:47:07 +0800

    docs: deploy

--- docs/assets/chunks/alipay3.c5o3sI-L.js
+++
@@ -1,4 +0,0 @@
-const _imports_0 = "/assets/apps/alipay3.webp";
-export {
-  _imports_0 as _
-};
--- docs/assets/chunks/constants.Dbpo2ZGA.js
+++
@@ -1,5 +0,0 @@
-import { K as withBase } from "./framework.Ch1t4NEC.js";
-const defaultAvatar = withBase("/default.png");
-export {
-  defaultAvatar as d
-};
